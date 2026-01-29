d9949f96c41f56cfb60c603709909f0751806b13
dnieln7 <dnieln7@gmail.com>
2026-01-29 16:23:03 -0600

    added missing home route

--- lib/main/presentation/screen/main.dart
+++ lib/main/presentation/screen/main.dart
@@ -21,12 +21,15 @@ import 'package:rook_flutter_demo/feature/connections/presentation/screen/connec
 import 'package:rook_flutter_demo/feature/connections/presentation/screen/connections_screen.dart';
 import 'package:rook_flutter_demo/feature/healthconnect/presentation/screen/health_connect_cubit.dart';
 import 'package:rook_flutter_demo/feature/healthconnect/presentation/screen/health_connect_screen.dart';
+import 'package:rook_flutter_demo/feature/home/presentation/screen/home_screen.dart';
 import 'package:rook_flutter_demo/feature/login/presentation/screen/login_cubit.dart';
 import 'package:rook_flutter_demo/feature/login/presentation/screen/login_screen.dart';
 import 'package:rook_flutter_demo/feature/postsplash/presentation/screen/post_splash_screen.dart';
 import 'package:rook_flutter_demo/feature/privacy/presentation/screen/privacy_policy_screen.dart';
 import 'package:rook_flutter_demo/feature/samsunghealth/presentation/screen/samsung_health_cubit.dart';
 import 'package:rook_flutter_demo/feature/samsunghealth/presentation/screen/samsung_health_screen.dart';
+import 'package:rook_flutter_demo/feature/settings/presentation/screen/settings_cubit.dart';
+import 'package:rook_flutter_demo/feature/summaries/presentation/screen/summaries_cubit.dart';
 import 'package:rook_flutter_demo/feature/welcome/presentation/screen/welcome_screen.dart';
 import 'package:rook_flutter_demo/main/presentation/screen/main_cubit.dart';
 import 'package:rook_flutter_demo/rook/repository/rook_apple_health_repository.dart';
@@ -231,6 +234,22 @@ final _router = GoRouter(
           );
         },
         child: AppleHealthScreen(),
+      ),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => SummariesCubit()),
+          BlocProvider(
+            create: (context) => SettingsCubit(
+              logger: context.read<Logger>(),
+              preferences: context.read<AppPreferences>(),
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+        ],
+        child: HomeScreen(),
       ),
     ),
   ],
